c535011446de06408e8a969572dd500c0e6c7578
Karol Gurba <karolgurba22@gmail.com>
2020-09-26 19:48:18 -0500

    created Student class

---
+++ Student.java
@@ -0,0 +1,21 @@
+// This class will be creating the Student Objects//
+// Name, Grade, Fees Total, Fees Paid
+
+public class Student {
+  private int id;
+  private String name;
+  private int grade;
+  private int feesPaid;
+  private int feesTotal;
+
+  // constructor // 
+  public Student(int id, String name, int grade) {
+    this.id=id;
+    this.name=name;
+    this.grade=grade;
+    this.feesTotal = 50000;
+    this.feesPaid = 0;
+
+  }
+
+}
