57f86e718b404cce91f758550e0bbae69c140bf0
Karol Gurba <karolgurba22@gmail.com>
2020-09-29 21:19:41 -0500

    created School class

---
+++ School.java
@@ -0,0 +1,10 @@
+import java.utils.List;
+
+public class School {
+
+  private List<Teacher> teachers;
+  private List<Student> students;
+  private int totalMoneyEarned;
+  private int totalMoneySpent;
+
+}
