65b7b05ca63b018345b4e9eeeb765df5564896f9
Karol Gurba <karolgurba22@gmail.com>
2020-09-29 20:29:37 -0500

    created teacher class, added constructor and get methods

---
+++ Teacher.java
@@ -0,0 +1,31 @@
+//This class will be creating Teacher Objects //
+// Id, name, salary //
+
+public class Teacher {
+  private int id;
+  private String name;
+  private int salary;
+
+  public Teacher(int id, String name, int salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+  }
+
+  
+  public int getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getSalary() {
+    return this.salary;
+  }
+
+  public void setSalary(int salary) {
+    this.salary = salary;
+  }
+}
