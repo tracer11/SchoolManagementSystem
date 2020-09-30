31ff94402cebff17453f07386378157c5e467e6a
Karol Gurba <karolgurba22@gmail.com>
2020-09-30 18:06:26 -0500

    added Main class

---
+++ Main.java
@@ -0,0 +1,8 @@
+package SchoolManagementSystem
+
+public class Main {
+  public static void main(String[] args) {
+    Teacher agata = new Teacher(id:1, name:"Agata", salary:60000);
+    Teacher aneta = new Teacher(id:2, name:"Aneta", salary:55000);
+  }
+}
