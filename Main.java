146464eeb99d13923b18eb138798a79818fd33dd
Karol Gurba <karolgurba22@gmail.com>
2020-09-30 18:25:56 -0500

    created new teacher, student and school objects

--- Main.java
+++ Main.java
@@ -4,5 +4,19 @@ public class Main {
   public static void main(String[] args) {
     Teacher agata = new Teacher(id:1, name:"Agata", salary:60000);
     Teacher aneta = new Teacher(id:2, name:"Aneta", salary:55000);
+
+    List<Teacher> teacherList = new ArrayList<>();
+    teacherList.add(agata);
+    teacherList.add(aneta);
+
+
+    Student karol = new Student(id:1, name:"Karol", grade:12);
+    Student jurek = new Student(id:2, name:"Jurek", grade:11);
+
+    List<Student> studentList = ArrayList<>();
+    studentList.add(karol);
+    studentList.add(jurek);
+
+    School cahs = new School(teacherList, studentList);
   }
 }
